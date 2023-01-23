4b2bca9535682c9bf0d6e3c04f0d0a4dedad91b6
nyein2001 <nyeinchantoe0@gmail.com>
2023-01-23 10:23:53 +0630

    Initial commit

---
+++ src/main/java/com/example/thutabookstore/ThutaBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.thutabookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThutaBookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThutaBookStoreApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/thutabookstore/entitiy/Book.java
@@ -0,0 +1,7 @@
+package com.example.thutabookstore.entitiy;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Book extends IdClass{
+}
